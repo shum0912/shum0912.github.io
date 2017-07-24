0413c02a1534d77da1993d5dc0dfc2573efc4b94
shum0912 <shum1209@ukr.net>
2017-07-24 09:51:02 +0300

    betrendStart

---
+++ betrend/other/share.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>stub page</title>
+
+</head>
+ <body bgcolor="#F5F5F5">
+<p>&nbsp;</p>
+<h3 style="text-align: center;"><font face="Verdana, Geneva, sans-serif">Шановний абоненте!</font></h3>
+<p>&nbsp;</p>
+
+<p>
+	<title></title>
+	<style type="text/css">div { 
+    width: 70%; /* Ширина */
+    background: #F5F5F5; /* Цвет фона */
+    padding: 20px; /* Поля */
+    margin: auto;
+    -moz-box-sizing: border-box; /* Для Firefox */
+    -webkit-box-sizing: border-box; /* Для Safari и Chrome */
+    box-sizing: border-box; /* Для IE и Opera */
+   }</style>
+</p>
+<div style="text-align: center;"><font face="Verdana, Geneva, sans-serif">Доступ на цей ресурс заблоковано згідно Указу Президента України №133/2017 Про рішення Ради національної безпеки і оборони України від 28 квітня 2017 року &quot;Про застосування персональних спеціальних економічних та інших обмежувальних заходів (санкцій)&quot;. </font></div>
+<div style="text-align: center;"><font face="Verdana, Geneva, sans-serif">Онлайн-підтримку та відповіді на запитання ви можете отримати на Порталі абонента <a href="http://my.volia.com">my.volia.com</a>, в спільноті ВоляКлуб <a href="http://club.volia.com">club.volia.com</a> та на офіційній сторінці ВОЛЯ у Facebook - <a href="http://facebook.com/voliaofficial">facebook.com/voliaofficial</a> . </font></div>
+<div style="text-align: center;"><font face="Verdana, Geneva, sans-serif">Також у наших спільнотах ви зможете отримувати вигідні пропозиції, новинки і цікавинки телеконтенту, технічні поради та лайфхаки, дізнаватися актуальні новини компанії тощо. </font></div>
+<div style="text-align: center;"><font face="Verdana, Geneva, sans-serif">З повагою, Компанія ВОЛЯ</font></div>
+
+<center>
+<img align="center" src="stub/logo.png" alt="logo"> 
+</center>
+</body>
+</html>
